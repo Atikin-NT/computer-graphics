800f663105c3ac4121fb334e2913a51bb8aba60b
AveZorgen <KulikovArt2015@gmail.com>
2023-02-21 16:03:08 +0300

    glass and waves filters

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from Filters.DotFilter import GrayScaleFilter, Sepia, LightCorrection
 from Filters.ContrastChage import ContrastFilter
 from Filters.BlurFilter import SharpFilter2
 from Filters.ContoursFilter import SobelFilter
+from Filters.WavesFilter import WavesFilter
 from PIL import Image
 
 
 def main():
     img = Image.open("data/img1.jpg")
     # filter = InvertFilter()
-    filter = LightCorrection()
+    filter = WavesFilter()
     resultImage = filter.processImage(img)
     resultImage.show()
 
