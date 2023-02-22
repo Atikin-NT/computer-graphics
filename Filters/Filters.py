fad792066491ae329892d6c89381b99524ea4820
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2023-02-22 16:12:28 +0300

    fix: ООП filters and contrast

--- Filters/Filters.py
+++ Filters/Filters.py
@@ -3,36 +3,21 @@ from abc import ABCMeta, abstractmethod
 from skimage import io
 import numpy as np
 
+
 class Filter(metaclass=ABCMeta):
     """Класс филтров"""
     @abstractmethod
-    def calculateNewPixelColor(self, sourceImage: np.ndarray, x: int, y: int, avg=0):
+    def calculateNewPixelColor(self, sourceImage: np.ndarray, x: int, y: int):
         pass
 
     def processImage(self, sourceImage: np.ndarray):
         resultImage = np.copy(sourceImage)
-        avg = self.avg(resultImage)
 
         width, height, _ = resultImage.shape
         for i in range(width):
             for j in range(height):
-                resultImage[i][j] = self.calculateNewPixelColor(sourceImage, i, j, avg)
+                resultImage[i][j] = self.calculateNewPixelColor(sourceImage, i, j)
         return resultImage
-
-    def avg(self, sourceImage: np.ndarray):
-        resultR = 0
-        resultG = 0
-        resultB = 0
-
-        width, height, _ = sourceImage.shape
-
-        for i in range(width):
-            for j in range(height):
-                neighborColor = sourceImage[i][j]
-                resultR += neighborColor[0]
-                resultG += neighborColor[1]
-                resultB += neighborColor[2]
-        return (1/3 * resultR + 1/3 * resultG + 1/3 * resultB) / (width * height)
 
     def Clam(self, value: int, min: int, max: int) -> int:
         if value < min:
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Filters.InvertFilter import InvertFilter
-# from Filters.ContrastChage import ContrastFilter
+from Filters.ContrastChage import ContrastFilter
 from Filters.BlurFilter import BlurrFilter
+from Filters.GaussianFilter import GaussianFilter
 from Filters.Transfer import Transfer
 from Filters.Turn import Turn
-from PIL import Image
 # from PIL import Image
 
 # import numpy as np
@@ -13,12 +13,18 @@ import matplotlib.pyplot as plt
 
 def main():
     img = io.imread("data/img1.jpg")
-    # filter = InvertFilter()
-    # filter = ContrastFilter()
-    # filter = BlurrFilter()
-    # filter = Transfer(100, 70)
-    filter = Turn(50, 50, 50)
+    filter = InvertFilter()
     resultImage = filter.processImage(img)
+    filter = ContrastFilter(2, img)
+    resultImage = filter.processImage(resultImage)
+    filter = BlurrFilter()
+    resultImage = filter.processImage(resultImage)
+    # filter = GaussianFilter(3, 5)
+    # resultImage = filter.processImage(resultImage)
+    filter = Transfer(50, 70)
+    resultImage = filter.processImage(resultImage)
+    filter = Turn(50, 50, 50)
+    resultImage = filter.processImage(resultImage)
     plt.imshow(resultImage)
     plt.show()
 
