56f88c9dba3f3d286e0c717109d599015146c30b
NikitaKorablev <kit06092003@gmail.com>
2023-03-08 17:08:40 +0300

    rework linear stretching

--- Filters/LinearStretching.py
+++ Filters/LinearStretching.py
@@ -5,6 +5,15 @@ import numpy as np
 
 class LinearStretching(Filter):
     def __init__(self, sourseImage: np.ndarray):
+        minR = 255
+        minG = 255
+        minB = 255
+        maxR = 0
+        maxG = 0
+        maxB = 0
+
+
+
         gist = Gistogramm(sourseImage)
 
         self.minBrightness = gist.getMinBrightness()
@@ -14,12 +23,12 @@ class LinearStretching(Filter):
         pixelColor = sourceImage[x][y]
         # print(pixelColor)
 
-        R = (pixelColor[0] - self.minBrightness) * (255 / (self.maxBrightness - self.minBrightness))
-        G = (pixelColor[1] - self.minBrightness) * (255 / (self.maxBrightness - self.minBrightness))
-        B = (pixelColor[2] - self.minBrightness) * (255 / (self.maxBrightness - self.minBrightness))
+        # R = (pixelColor[0] - self.minBrightness) * (255 / (self.maxBrightness - self.minBrightness))
+        # G = (pixelColor[1] - self.minBrightness) * (255 / (self.maxBrightness - self.minBrightness))
+        # B = (pixelColor[2] - self.minBrightness) * (255 / (self.maxBrightness - self.minBrightness))
 
         newPixelColor = (pixelColor - self.minBrightness) * (255 / (self.maxBrightness - self.minBrightness))
-        return (self.Clamp(R, 0, 255),
-                self.Clamp(G, 0, 255),
-                self.Clamp(B, 0, 255))
-        # return newPixelColor
+        # return (self.Clamp(R, 0, 255),
+        #         self.Clamp(G, 0, 255),
+        #         self.Clamp(B, 0, 255))
+        return self.Clamp(newPixelColor[0], 0, 255)
